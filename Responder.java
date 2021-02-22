f057750ac0f33ab36d8d7805c4b1afba7426a0d7
cristobal <cyarari@gmail.com>
2021-02-22 10:04:18 +0100

    Respondiendo a conjuntos de respuestas

--- Responder.java
+++ Responder.java
@@ -14,7 +14,11 @@ public class Responder
 {
     private Random aleatorio;
     private ArrayList<String> cadena;
-    private HashMap <String, String> respuestas;
+    private HashMap <HashSet<String>, String> respuestas;
+    private HashSet <String> respuesta1;
+    private HashSet <String> respuesta2;
+    private HashSet <String> respuesta3;
+    private HashSet <String> respuesta4;
      
     /**
      * Construct a Responder - nothing to do
@@ -24,15 +28,29 @@ public class Responder
         aleatorio = new Random();
         cadena = new ArrayList<String>();
         respuestas = new HashMap<>();
+        respuesta1 = new HashSet <>();
+        respuesta2 = new HashSet <>();
+        respuesta3 = new HashSet <>();
+        respuesta4 = new HashSet <>();
         cadena.add("¿Que tal te ha ido el dia?");
         cadena.add("Es interesante");
         cadena.add("Gracias!!");
         cadena.add("A mi tambien me gusta eso.");
         cadena.add("Eres muy agradable");
-        respuestas.put("bien", "Me alegro, a mi tambien me ha ido bien");
-        respuestas.put("triste", "¿Por que estas triste?");
-        respuestas.put("problema", "Cuentamelo, quiza puedo ayudarte");
-        respuestas.put("placer", "El placer es mio");
+        respuesta1.add("estoy");
+        respuesta1.add("bien");
+        respuesta2.add("me");
+        respuesta2.add("encuentro");
+        respuesta2.add("triste");
+        respuesta3.add("tengo");
+        respuesta3.add("un");
+        respuesta3.add("problema");
+        respuesta4.add("eres");
+        respuesta4.add("agradable");
+        respuestas.put(respuesta1, "Me alegro, a mi tambien me ha ido bien");
+        respuestas.put(respuesta2, "¿Por que estas triste?");
+        respuestas.put(respuesta3, "Cuentamelo, quiza puedo ayudarte");
+        respuestas.put(respuesta4, "Gracias, tu tambien eres agradable");
     }
 
     /**
@@ -42,13 +60,7 @@ public class Responder
     public String generateResponse(HashSet<String> userInput)
     {
         String resultado = null;
-        Iterator <String> ite = userInput.iterator();
-        String cadenas = "";
-        while (resultado == null && ite.hasNext()){
-            cadenas = ite.next();
-            resultado = respuestas.get(cadenas);
-        }
-        
+        resultado = respuestas.get(userInput);
         if (resultado == null){
             int aleat = aleatorio.nextInt(cadena.size());
             resultado = cadena.get(aleat);
